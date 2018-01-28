7ef6369b45acbb5cef3ab163ca4d0b584fd39a74
David Weatherall <davidweatherall95@gmail.com>
2018-01-28 22:11:43 +0000

    Updated pro data set and added new pro scripts

---
+++ Pro/blueTower.py
@@ -0,0 +1,26 @@
+import urllib.request
+import os
+import json
+import math
+
+
+y = 0
+z = 0
+
+path = 'game/'
+games = os.listdir(path)
+
+for game in games:
+	y += 1
+	file = 'game/' + game
+
+	f = open(file, 'r')
+	text = f.read()
+	encjson = json.loads(text)
+
+	if(encjson['teams'][0]['firstTower'] is True):
+		z += 1
+
+os.system('clear')
+print('Blue side FT is {}%. Sample size: {}'.format(str(round((z/y)*100)), str(y)))
+print('Red side FT is {}%. Sample size: {}'.format(str(100 - round((z/y)*100)), str(y)))
